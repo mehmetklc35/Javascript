12fcfb1e9ce4b2d8e7f2975d2b7a63febcda0bd9
mehmet kılıç <mehmetkilic35@outlook.com>
2024-02-05 17:48:52 +0300

    img düzeltmesi yapıldı

--- Javascript_Projeler/progres_bar.js
+++ Javascript_Projeler/progres_bar.js
@@ -8,28 +8,28 @@ let user = [
     {
         name: 'Mehmet KILIÇ',
         position: 'IT',
-        photo: 'http',
+        photo: '/img/mehmet.png',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil corrupti quidem, ut commodi quas, architecto, soluta aliquid minus dolorem sint consectetur in iure amet eaque doloribus. Tempore voluptatem sint iste.',   
     
     },
     {
         name: 'Berkan ÖZMEN',
         position: 'IT',
-        photo:'http:',
+        photo:'/img/berkan.png',
         text:'Lorem Ipsum dolor sit amet, consectetur adipiscing elit nilibrium et just euismod tempor inv sapien soluta nobis et just euismod tempor inviw',  
     
     },
     {
         name: 'Sude AKÇETUTAN',
         position: 'IT',
-        photo:'https',
+        photo:'/img/sude.png',
         text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum accusantium harum est nulla perferendis placeat expedita ad deleniti fugit eveniet impeditdi.',
     
     },
     {
         name: 'Ahmet İLÇİ',
         position: 'IT',
-        photo:'htt',
+        photo:'/img/ahmet.png',
         text: 'lorem Ips et et ullamcorper et ullamcorper et ullamcorper et ullamcorper et ullamcorper et ullam correlation null hypothesis et et et et et et et et et et et et et et et et  ',
     
     },
